548204cc6749414309493488eb900071a86fc8a0
Matthew Tichborne <67195167+mjt12@users.noreply.github.com>
2026-02-25 14:30:34 +0000

    Update main.py
    Getting backbone down

--- main.py
+++ main.py
@@ -76,9 +76,66 @@ List with player type, noise parameter and net gain
 Check zero sum game
 """
 
+# Parameters:
 
-  
-  
+
+n_strategic_agents = 100
+n_zi_agents = 100
+n_rounds = 10
+S0 = 100
+volatility = 0.02
+drift = 0
+total_initial_shares = 100
+cash_to_share_ratio = 100
+distribution_of_players = 'normal'
+distribution_of_noise = 'uniform'
+run_id = 1
+
+
+class game:
+    def __init__(self, n_strategic_agents, n_zi_agents, n_rounds, S0, volatility, drift, initial_quantity, initial_cash, run_id):
+        self.current_round = 0
+        self.players = {}
+        self.sigma = sigma
+        self.n_strategic_agents = n_strategic_agents
+        
+
+        # Order history: tuple {current_round : {buy_orders, sell_orders}}
+        self.order_history = {}
+        self.price_history = {}
+
+        # Creates N lots of informed traders, with decreasing information on next price as index increases
+        for i in range(M_n_players):
+            self.players[i] = trader(
+                identity=i, 
+                information=i * sigma / N_i_players, 
+                starting_cash=starting_cash, 
+                starting_quantity=starting_quantity)
+
+        # Creates M noise traders
+        for i in range(M_n_players):
+            self.players[i + N_i_players] = trader(i + N_i_players, 
+                                                   -1, 
+                                                   starting_quantity, 
+                                                   starting_cash)
+        self.stock_path = simulate_gbm(S_0, sigma, M_rounds)
+
+
+
+Parameters:
+- Number of signal following agents: n_strategic_agents
+- Number of noisy following agents: n_zi_agents
+- Number of rounds: n_rounds
+- Initial underlying: S0
+- Volatility: volatility
+- Drift: drift
+- Total initial shares : total_initial_shares
+- Cash to asset ratio : cash_to_share_ratio
+- Distribution of players : information_distribution in {uniform, bimodal, lognormal}
+- Distribution of noise: noise_distribution in {lognormal, uniform, ...}
+- Run id: ulid
+
+
 
   
   
